212c5f3600d4e6bfc182e08291d34c2b87d6a6a2
Dhruv <dhruvjain0506@gmail.com>
2026-09-14 12:24:29 +0530

    built a general network and implemented forward pass with linear layers and activation

---
+++ model.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+def relu(Z):
+    return np.maximum(0, Z)
+
+def sigmoid(Z):
+    return 1 / (1 + np.exp(-Z))
+
+class SimpleNN:
+    def __init__(self, input_size, hidden_size, output_size):
+        self.W1 = np.random.randn(input_size, hidden_size) * 0.01
+        self.b1 = np.zeros((1, hidden_size))
+        self.W2 = np.random.randn(hidden_size, output_size) * 0.01
+        self.b2 = np.zeros((1, output_size))
+
+    def forward(self, X):
+        self.Z1 = X @ self.W1 + self.b1
+        self.A1 = relu(self.Z1)
+        self.Z2 = self.A1 @ self.W2 + self.b2
+        self.A2 = sigmoid(self.Z2)
+        return self.A2
